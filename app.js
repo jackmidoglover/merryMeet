890f99d41229a1d13f107daa44d1a8cfe032f723
Jacquelyne Glover <jackmidoglover@gmail.com>
2018-07-26 15:37:29 -0600

    typo

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use('/api', indexRouter);
 app.use('/api/users', usersRouter);
 
 app.get('*', (req,res) => {
-  res.sendFile(path.join(__dirname + 'public/index.html'));
+  res.sendFile(path.join(__dirname + '/public/index.html'));
 })
 
 
