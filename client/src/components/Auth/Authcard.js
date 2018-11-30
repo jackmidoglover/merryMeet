fd25a42d85e7af56007f95473ea76bf5f9185a62
Jacquelyne Glover <jackmidoglover@gmail.com>
2018-11-30 00:52:13 -0700

    beginning to style MerryMeet

--- client/src/components/Auth/Authcard.js
+++ client/src/components/Auth/Authcard.js
@@ -1,14 +1,14 @@
 import React from 'react';
-
+import './authcard.css';
 
 const Authcard = props => {
 return (
 <div className="card authScreen col-md-4 offset-md-4">
     <div className="card-body row">
         <div className="title col-md-6 offset-md-3">
-            <h3 className="card-title text-center">
+            <h2 className="card-title text-center">
                 {props.title}
-            </h3>
+            </h2>
         </div>
         </div>
             <div className="login-group row">
--- client/src/components/Wrapper.jsx
+++ client/src/components/Wrapper.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './wrapper.css';
 
 const Wrapper = props => {
     return(
         <div className="container-fluid">
-        <header className="header text-center"> <h1> Merry Meet </h1> </header>
+        <header className="header text-center"> <h1 class="logo"> Merry Meet </h1> </header>
             {props.children}
         </div>
     )
