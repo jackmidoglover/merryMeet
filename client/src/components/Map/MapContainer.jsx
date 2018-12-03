0a9c79fb68418cd9a6c94ee0de2d882f7d54214a
Jacquelyne Glover <jackmidoglover@gmail.com>
2018-12-03 11:54:26 -0700

    user window component initiated, accepting props from mapContainer component

--- client/src/components/Map/MapContainer.jsx
+++ client/src/components/Map/MapContainer.jsx
@@ -170,5 +170,5 @@ const style = {
     marginBottom: "20px",
     position: "relative",
     backgroundColor: "#311b92",
-    border: "10px solid #192261"
+    border: "10px solid #C24B3E"
 }
--- client/src/components/Map/UserWindow.jsx
+++ client/src/components/Map/UserWindow.jsx
@@ -7,11 +7,14 @@ export class UserWindow extends React.Component {
 
     render(){
         return(
-            <div className="col-md-4 mt-2" style={{backgroundColor: "#34144F"}}>
+            <div className="col-md-4 mt-2" style={{backgroundColor: "#A43640", position: "absolute"}}>
             <h1>Hello, {this.props.user.username}!</h1>
             <hr />
             <h2>Your Profile: </h2>
-                
+                <p>Username: {this.props.user.username}</p>
+                <p>Religion: {this.props.user.religion}</p>
+                <p>Zipcode: {this.props.user.zipcode}</p>
+                <p>Biography: {this.props.user.bio}</p>
             </div>
         )
     }
