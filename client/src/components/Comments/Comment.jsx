fc131db12526756ef445edf1cc13906bb65b4df2
Jacquelyne Glover <jackmidoglover@gmail.com>
2018-12-19 16:20:05 -0700

    comment box styling

--- client/src/components/Comments/Comment.jsx
+++ client/src/components/Comments/Comment.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const CommentBox = props => {
     return (
         <div className="row" style={{borderBottom: "dotted #E8D0DE 2px"}}>
-            <div className="col-md-3 userInfo" style={{borderRight: "dotted #E8D0DE 2px"}}>
+            <div className="col-md-4 userInfo" style={{borderRight: "dotted #E8D0DE 2px"}}>
                 <div className="row">
                     <div className="col-md-12">
                         <span className="text-center">
@@ -17,7 +17,7 @@ const CommentBox = props => {
                     <img src={props.userImage} alt={props.username} className="img img-circle mb-3" style={{ width: "100px", height: "100px", margin: "auto" }} />
                 </div>
             </div>
-            <div className="col-md-9 commentInfo">
+            <div className="col-md-8 commentInfo">
                 <p className="mt-4"> {props.text} </p>
             </div>
             <hr />
